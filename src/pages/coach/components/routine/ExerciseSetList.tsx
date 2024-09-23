897f91321b259a0dd3d5a5b7dde93f3ad54a8a13
Diegocarque12 <diegocarque1213@gmail.com>
2024-09-23 13:52:42 -0600

    feat(weight-types): available to show the name of the weight type

--- src/pages/coach/components/routine/ExerciseSetList.tsx
+++ src/pages/coach/components/routine/ExerciseSetList.tsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import ExerciseSetForm from './ExerciseSetForm';
-import { Exercise, RoutineExercise, RoutineExerciseSet } from "interfaces/types";
+import { Exercise, RoutineExercise, RoutineExerciseSet, WeightTypes } from "interfaces/types";
+import { GetCompleteWeightType } from '@/utils/WeightType';
+import supabase from '@/utils/supabaseClient';
 
 interface ExerciseSetListProps {
     routineExercises: RoutineExercise[];
@@ -24,8 +26,32 @@ const ExerciseSetList = ({
     onDeleteExercise,
     onAdd,
 }: ExerciseSetListProps) => {
+    const [, setWeightType] = useState<WeightTypes>();
     const [editingSet, setEditingSet] = useState<RoutineExerciseSet | null>(null);
     const [isAddingNew, setIsAddingNew] = useState(false);
+    const [isWeightTypeDialogOpen, setIsWeightTypeDialogOpen] = useState(false);
+    const [refreshPage, setRefreshPage] = useState(false);
+
+    const onUpdateWeightType = async (routineExerciseId: number, newWeightTypeId: number) => {
+        setWeightType({
+            id: newWeightTypeId,
+            name: GetCompleteWeightType(newWeightTypeId),
+        });
+        console.log(newWeightTypeId, routineExerciseId);
+
+        const { error } = await supabase.from('routine_exercises').update({ weight_type_id: newWeightTypeId }).eq('routine_id', routineExerciseId);
+        if (error) {
+            console.error('Error updating weight type:', error);
+        }
+        setIsWeightTypeDialogOpen(false);
+        setRefreshPage(true);
+    };
+
+    const weightTypes = [
+        { id: 1, name: 'Kilos' },
+        { id: 2, name: 'Libras' },
+        { id: 3, name: 'Pesas' },
+    ];
 
     return (
         <div>
@@ -36,15 +62,64 @@ const ExerciseSetList = ({
                     <div key={routineExercise.id} className="mb-6 border p-4 rounded-lg">
                         <div className="flex justify-between items-center mb-4">
                             <h3 className="text-xl font-bold mb-2">{exercise?.name}</h3>
-                            <button className='bg-red-500 p-4 text-white rounded-lg' onClick={() => onDeleteExercise(routineExercise.id)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                </svg>
+                            <div className='flex items-center'>
+                                {refreshPage && (
+                                    <Button
+                                        onClick={() => window.location.reload()}
+                                        className="mr-2 bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md transition duration-300 ease-in-out"
+                                    >
+                                        Refrescar Página
+                                    </Button>
+                                )}
+                                <Dialog open={isWeightTypeDialogOpen} onOpenChange={setIsWeightTypeDialogOpen}>
+                                    <DialogTrigger asChild>
+                                        <Button variant="secondary" className="mr-2 bg-blue-600 text-white hover:bg-blue-700">Cambiar Tipo de Peso</Button>
+                                    </DialogTrigger>
+                                    <DialogContent>
+                                        <DialogHeader>
+                                            <DialogTitle>Cambiar el Tipo de Peso</DialogTitle>
+                                        </DialogHeader>
+                                        <div className="flex flex-col space-y-4">
+                                            <form onSubmit={(e) => {
+                                                e.preventDefault();
+                                                const formData = new FormData(e.target as HTMLFormElement);
+                                                const newWeightTypeId = Number(formData.get('weightType'));
+                                                onUpdateWeightType(routineExercise.routine_id, newWeightTypeId);
+                                            }}>
+                                                <div className="flex flex-col space-y-2">
+                                                    <label htmlFor="weightType" className="text-sm font-medium text-gray-700">
+                                                        Tipo de Peso
+                                                    </label>
+                                                    <select
+                                                        id="weightType"
+                                                        name="weightType"
+                                                        defaultValue={routineExercise.weight_type_id}
+                                                        className="w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                                                    >
+                                                        {weightTypes.map((type) => (
+                                                            <option key={type.id} value={type.id}>
+                                                                {type.name}
+                                                            </option>
+                                                        ))}
+                                                    </select>
+                                                </div>
+                                                <Button type="submit" className="w-full mt-4 bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                                                    Guardar
+                                                </Button>
+                                            </form>
+                                        </div>
+                                    </DialogContent>
+                                </Dialog>
+                                <button className='bg-red-500 p-4 text-white rounded-lg' onClick={() => onDeleteExercise(routineExercise.id)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                        <polyline points="3 6 5 6 21 6"></polyline>
+                                        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+                                        <line x1="10" y1="11" x2="10" y2="17"></line>
+                                        <line x1="14" y1="11" x2="14" y2="17"></line>
+                                    </svg>
+                                </button>
+                            </div>
 
-                            </button>
                         </div>
                         <table className="w-full border-collapse">
                             <thead>
@@ -52,6 +127,7 @@ const ExerciseSetList = ({
                                     <th className="border p-2 text-left">Set</th>
                                     <th className="border p-2 text-left">Peso sugerido</th>
                                     <th className="border p-2 text-left">Repeticiones sugeridas</th>
+                                    <th className="border p-2 text-left">Tipo de peso</th>
                                     <th className="border p-2 text-left">Acciones</th>
                                 </tr>
                             </thead>
@@ -61,6 +137,7 @@ const ExerciseSetList = ({
                                         <td className="border p-2">{set.set_number}</td>
                                         <td className="border p-2">{set.suggested_weight}</td>
                                         <td className="border p-2">{set.suggested_repetitions}</td>
+                                        <td className="border p-2">{GetCompleteWeightType(routineExercise.weight_type_id)}</td>
                                         <td className="border p-2">
                                             <Dialog open={(editingSet?.id === set.id) || (isAddingNew && !set.id)}
                                                 onOpenChange={(open) => {
@@ -97,7 +174,7 @@ const ExerciseSetList = ({
                                     </tr>
                                 ))}
                                 <tr>
-                                    <td colSpan={4}>
+                                    <td colSpan={5}>
                                         <Button className="w-full" onClick={() => onAdd(routineExercise.id)}>
                                             Agregar Set
                                         </Button>
